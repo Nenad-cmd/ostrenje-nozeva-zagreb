63969bbde38b4a3b54062ebe11fc8a3e44defeab
Nenad-cmd <brus.lab3@gmail.com>
2026-02-06 12:07:35 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+app/page.tsx
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Oštrenje noževa Zagreb</h1>
+      <p>Stranica je uspješno postavljena. Uskoro više sadržaja.</p>
+    </main>
+  );
+}
+
